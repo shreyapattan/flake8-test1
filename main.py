30fd037c033973e9375980d93bcc9a8e414ebb48
shreya pattan <shreyapattan99@gmail.com>
2021-11-17 17:13:27 +0530

    introduced errors

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ num1 = 1
 num2 = 6
 
 # Add two numbers
-sum = num1 + num2
+sum = nu1 + num2
 sum = num1 + num2
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'. format(num1, num2, sum))
